22b617ebe57bc162288c31e23e77d1b3501401ca
Datoclement <70664914@qq.com>
2017-02-13 18:32:37 +0100

    setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# FoodDeliverySwift
+
+To build an application for the commerce of delivery of Chinese food
---
+++ myvirualenv/foodtasker/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Datoclement1/Documents/Xproject/tutorial/myvirualenv/foodtasker/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
