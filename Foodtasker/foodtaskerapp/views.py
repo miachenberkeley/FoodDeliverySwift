89baa37f780f1bdd8c95d2394829e01d683f26d6
Datoclement <70664914@qq.com>
2017-02-17 17:54:23 +0100

    Register

---
+++ Foodtasker/foodtaskerapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def home(request):
+    return redirect(restaurant_home)
+
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_home(request):
+    return render(request, 'restaurant/home.html', {})
+
+def restaurant_sign_up(request):
+    return render(request, 'restaurant/sign_up.html', {})
